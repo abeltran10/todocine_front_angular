f0a5b39fbc26cf0c4eb2f20da842c71dfd3d0d39
abeltran10 <you@example.com>
2026-01-29 20:52:49 +0100

    added cinema

--- src/app/core/enum/cines.ts
+++ src/app/core/enum/cines.ts
@@ -1,4 +1,4 @@
-export type CinesKey = 1 | 2;
+export type CinesKey = 1 | 2 | 3;
 
 export interface Cine {
   id: number;
@@ -9,7 +9,8 @@ export interface Cine {
 
 const CINES: Record<CinesKey, Cine> = {
    1: {id: 1, name: 'Cinesa', url: 'https://www.cinesa.es/', region: 'ES'},
-   2: {id: 2, name: 'Cines Sucre', url: 'https://www.sucrecines.com/', region: 'ES'}
+   2: {id: 2, name: 'Cines Sucre', url: 'https://www.sucrecines.com/', region: 'ES'},
+   3: {id: 3, name: 'AMC Theatres', url: 'https://www.amctheatres.com/', region: 'US'}
 };
 
 export const Cines = {
